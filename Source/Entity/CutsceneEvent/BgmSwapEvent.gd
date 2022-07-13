1c1ad364a3e3e9de3fcc98f249f93a0e11d418dd
jumpy-jumper <42717910+jumpy-jumper@users.noreply.github.com>
2022-07-12 21:58:41 -0400

    Improve fade event interface

--- Source/Entity/CutsceneEvent/BgmSwapEvent.gd
+++ Source/Entity/CutsceneEvent/BgmSwapEvent.gd
@@ -1,10 +1,15 @@
 tool
 extends CutsceneEvent
 
+
+enum Duration {INSTANT=0, SONIC=1, FAST=2, NORMAL=3, SLOW=4, VERY_SLOW=5}
+
 export var bgm := ""
-export var crossfade_in := 0.0
-export var crossfade_out := 0.25
+export (Duration) var crossfade_out := Duration.FAST
+export (Duration) var crossfade_in := Duration.INSTANT
 
 func execute():
 	.execute()
+	crossfade_in = [0, 0.25, 0.5, 1, 2, 4][crossfade_in]
+	crossfade_out = [0, 0.25, 0.5, 1, 2, 4][crossfade_out]
 	Song.change_bgm(bgm if bgm != "" else null, crossfade_out, crossfade_in)
--- Source/Entity/CutsceneEvent/FadeEvent.gd
+++ Source/Entity/CutsceneEvent/FadeEvent.gd
@@ -8,15 +8,15 @@ export (Fade) var fade := Fade.FADE_IN
 enum Duration {INSTANT=0, SONIC=1, FAST=2, NORMAL=3, SLOW=4, VERY_SLOW=5}
 export (Duration) var duration := Duration.NORMAL
 
-export (NodePath) onready var node = get_node(node)
+export (NodePath) onready var node_to_fade = get_node(node_to_fade)
 
 func _ready():
-	if node and fade==Fade.FADE_IN:
-		node.modulate.a = 0
+	if node_to_fade and fade==Fade.FADE_IN:
+		node_to_fade.modulate.a = 0
 
 func execute():
 	var t = [0, 0.25, 0.5, 1, 2, 4][duration]
-	Global.tween(node if node else Global.cutscene, "modulate:a", \
+	Global.tween(node_to_fade if node_to_fade else Global.cutscene, "modulate:a", \
 		0 if fade == Fade.FADE_IN else 1, 1 if fade == Fade.FADE_IN else 0, t)
 	if wait_for_fade:
 		wait += t
