1e7c85a5d623ece5025cec1c9aefd47243d27688
jumpy-jumper <42717910+jumpy-jumper@users.noreply.github.com>
2022-07-13 10:34:37 -0400

    Implement player movement

--- Source/Entity/Player/Player.gd
+++ Source/Entity/Player/Player.gd
@@ -2,3 +2,13 @@ extends Node2D
 
 func _ready_global():
 	Global.player = self
+
+# Movement
+
+var speed = 600
+
+func _physics_process_movement(delta):
+	var mov = Input.get_vector(\
+		"movement_x-", "movement_x+", "movement_y-", "movement_y+")
+
+	position += speed * mov * delta
